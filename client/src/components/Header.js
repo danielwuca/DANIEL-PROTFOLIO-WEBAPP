39a4a9d84e306495aef81dc45b54ab3bd11dea29
Zhenyu Wu <114626189+danielwuca@users.noreply.github.com>
2024-04-18 15:14:20 -0400

    Update Header.js
    add v1

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <div className="p-5 bg-primary flex justify-between">
-      <h2 className="text-secondary text-5xl font-semibold">Daniel Tech</h2>
+      <h2 className="text-secondary text-5xl font-semibold">Daniel Tech: v1</h2>
     </div>
   );
 }
